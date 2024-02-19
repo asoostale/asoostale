d35825f5684d01f6fc319948a18c662b02590ee3
SooYoung Joo <asoostale@naver.com>
2024-02-20 00:46:27 +0900

    프로젝트 생성 및 init

---
+++ src/main/java/study/asoostale/AsoostaleApplication.java
@@ -0,0 +1,13 @@
+package study.asoostale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsoostaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsoostaleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/asoostale/AsoostaleApplicationTests.java
@@ -0,0 +1,13 @@
+package study.asoostale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsoostaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
